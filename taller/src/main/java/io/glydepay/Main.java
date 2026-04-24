9157819177c65658787a585105ac004985309bb2
Swami Costa <swami@glydepay.io>
2026-04-24 16:07:04 -0300

    add imports

--- taller/src/main/java/io/glydepay/Main.java
+++ taller/src/main/java/io/glydepay/Main.java
@@ -1,10 +1,14 @@
 package io.glydepay;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        
+
         System.out.println("Result " + checkString("abcd", "cdab"));
     }
 
